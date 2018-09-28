5047d498a63c249f216203d3a135d2635fa48aa4
Yangyang Xia <Yangyang.Xia@ygomi.com>
2018-09-28 18:07:59 +0800

    update lucene basic demo

--- lucene-lab-basic/src/test/java/com/s2u2m/lab/lucene/basic/LucenceBasicOpsTest.java
+++ lucene-lab-basic/src/test/java/com/s2u2m/lab/lucene/basic/LucenceBasicOpsTest.java
@@ -1,10 +1,12 @@
 package com.s2u2m.lab.lucene.basic;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -20,30 +22,34 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 /**
- * AddDocumentTest
+ * LucenceBasicOpsTest
  * Create by Yangyang.xia on 9/11/18
  */
-public class AddDocumentTest {
+public class LucenceBasicOpsTest {
 
-    private static final String INDEX_DIR;
+    private static final URL indexDirUrl;
+    private String INDEX_DIR;
     static  {
-        URL indexDirUrl = LuceneLabBasicMain.class.getClassLoader().getResource("index");
-        INDEX_DIR = indexDirUrl.getPath();
+        indexDirUrl = LuceneLabBasicMain.class.getClassLoader().getResource("index");
     }
 
-    @Test
-    public void addDocumentTest() throws IOException {
+    private static final String idField = "id";
+    private static final String idValue = "1";
+
+    @Before
+    public void addDocument() throws IOException {
+        INDEX_DIR = String.join(File.separator, indexDirUrl.getPath(), UUID.randomUUID().toString());
+
         FSDirectory directory = FSDirectory.open(Paths.get(INDEX_DIR));
         IndexWriterConfig config = new IndexWriterConfig();
         IndexWriter writer = new IndexWriter(directory, config);
-
-        final String idField = "id";
-        final String idValue = "1";
 
         Document document = new Document();
         // add field to document
@@ -54,7 +60,23 @@ public class AddDocumentTest {
         writer.addDocument(document);
 
         writer.close();
+    }
 
+    @After
+    public void deleteDocument() throws IOException {
+        FSDirectory directory = FSDirectory.open(Paths.get(INDEX_DIR));
+        IndexWriterConfig config = new IndexWriterConfig();
+        IndexWriter writer = new IndexWriter(directory, config);
+
+        Query query = new TermQuery(new Term(idField, idValue));
+        writer.deleteDocuments(query);
+
+        writer.close();
+    }
+
+    @Test
+    public void query() throws IOException {
+        FSDirectory directory = FSDirectory.open(Paths.get(INDEX_DIR));
         IndexReader reader = DirectoryReader.open(directory);
         IndexSearcher searcher = new IndexSearcher(reader);
 
@@ -67,5 +89,7 @@ public class AddDocumentTest {
         String exactIdValue = doc.get(idField);
         assertEquals(idValue, exactIdValue);
     }
+
+
 
 }
